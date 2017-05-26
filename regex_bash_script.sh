2e53b056dc093af536384b7ee6456ae39543f61e
Elton Islamaj <elton.islamaj@gmail.com>
2017-05-26 19:20:05 -0400

    extracting data using regex
    Web scraping using regular expression in bash script with kali linux

--- regex_bash_script.sh
+++ regex_bash_script.sh
@@ -5,43 +5,43 @@ curl http://static.hurstathletics.com/custompages/MBasketball/2016-17/teamstat.h
 
 while read line
 do
-date_col=$(echo $line | egrep -o "^[0-9]+/[0-9]+/[0-9]+")
-# code below selects only the city name 
-city=$(echo $line | egrep -o "^[A-Z].+" | egrep -v ".[0-9]+" |
+      date_col=$(echo $line | egrep -o "^[0-9]+/[0-9]+/[0-9]+")
+      # code below selects only the city name 
+      city=$(echo $line | egrep -o "^[A-Z].+" | egrep -v ".[0-9]+" |
              cut -d "," -f 1 | cut -d "&" -f 1 | sed "s/University//")
-# code below selects only the state initials
-state=$(echo $line | egrep -o "^[A-Z].+" | egrep -v ".[0-9]+" | 
+      # code below selects only the state initials
+      state=$(echo $line | egrep -o "^[A-Z].+" | egrep -v ".[0-9]+" | 
               cut -d "," -f 2 | cut -d " " -f 2 | sed "s/a/A/" | 
               sed "s/Athletic//" | cut -d "." -f 1)
-# location variable retrives both city and state data. 
-location=$(echo $line | egrep -o "^[A-Z].+" | egrep -v ".[0-9]+" |
+      # location variable retrives both city and state data. 
+      location=$(echo $line | egrep -o "^[A-Z].+" | egrep -v ".[0-9]+" |
                  cut -d "(" -f 1 | cut -d "&" -f 1 | sed "s/,//" | cut -d "." -f 1 |
                  sed "s/McComb Fieldhouse//" | sed -e "s/Pa/PA/g")
-# where the games were played
-venue=$(echo $line | egrep -o "^[A-Z].+" | egrep -v ".[0-9]+" |
+      # where the games were played
+      venue=$(echo $line | egrep -o "^[A-Z].+" | egrep -v ".[0-9]+" |
               cut -d "(" -f 2 | cut -d ")" -f 1 | cut -d "&" -f 1 | cut -d "." -f 2 |
               sed "s/,//")
-# home team's name
-home=$(echo $line | egrep -a "[a-zA-Z] [0-9]" | cut -d "," -f 1 | 
+      # home team's name
+      home=$(echo $line | egrep -a "[a-zA-Z] [0-9]" | cut -d "," -f 1 | 
              sed -e "s/[0-9]//g" | sed -e "s/(//g" | sed -e "s/)//g" | sed "s/Pa./PA/")
-# home team's result
-home_result=$(echo $line | egrep -o "[0-9]+," | cut -d "," -f 1)
-# away team's name
-away=$(echo $line | egrep -a "[a-zA-Z] [0-9]" | cut -d "," -f 2 |
+      # home team's result
+      home_result=$(echo $line | egrep -o "[0-9]+," | cut -d "," -f 1)
+      # away team's name
+      away=$(echo $line | egrep -a "[a-zA-Z] [0-9]" | cut -d "," -f 2 |
              sed -e "s/[0-9]//g" | cut -d "&" -f 1 | sed -e "s/(//g" | sed -e "s/)//g")
-# away team's result
-away_result=$(echo $line | egrep -a "[a-zA-Z] [0-9]" | cut -d "," -f 2 |
+      # away team's result
+      away_result=$(echo $line | egrep -a "[a-zA-Z] [0-9]" | cut -d "," -f 2 |
                     cut -d "&" -f 1 | egrep -o "[0-9]{2}")
 
-echo $date_col 
-echo $city
-echo $state
-echo $location 
-echo $venue 
-echo $home 
-echo $away 
-echo $home_result 
-echo $away_result 
+      echo $date_col 
+      echo $city
+      echo $state
+      echo $location 
+      echo $venue 
+      echo $home 
+      echo $away 
+      echo $home_result 
+      echo $away_result 
 
 done  < basket_file.txt
 
